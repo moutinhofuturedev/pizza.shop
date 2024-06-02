fdbf61f267cc6a4e9866124d3b27d8bdca2b6da8
paulo <paulo_vicali@icloud.com>
2024-06-01 22:28:29 -0300

    feat(master): cria componente header com links
    O componente header foi desenvolvido com layout fixo para agrupar links de navegação dentro a
    aplicação

---
+++ src/components/nav-link/index.tsx
@@ -0,0 +1,15 @@
+import { Link, LinkProps, useLocation } from 'react-router-dom'
+
+export type NavLinkProps = LinkProps
+
+export const NavLink = (props: NavLinkProps) => {
+  const { pathname } = useLocation()
+
+  return (
+    <Link
+      {...props}
+      data-active={pathname === props.to}
+      className="flex items-center gap-2 text-sm font-medium text-muted-foreground hover:text-foreground data-[active=true]:text-foreground data-[active=true]:underline"
+    />
+  )
+}
