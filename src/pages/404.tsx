bea00a8029c2c22d8a8ca2b9c74fd89c6d72ae43
paulo <paulo_vicali@icloud.com>
2024-06-16 15:08:04 -0300

    feat(main): criação da página not found

---
+++ src/pages/404.tsx
@@ -0,0 +1,27 @@
+import { useNavigate } from 'react-router-dom'
+
+import { Button } from '@/components/ui/button'
+
+export const NotFound = () => {
+  const navigate = useNavigate()
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <div className="mx-auto max-w-screen-xl px-4 py-8 max-md:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <h1 className="mb-4 text-5xl font-extrabold tracking-tight max-lg:text-7xl">
+            Whoops!404
+          </h1>
+          <p className="tracking-tigh mb-4 text-3xl font-bold dark:text-white max-lg:text-4xl">
+            Página não encontrada.
+          </p>
+          <p className="mb-8 text-lg font-light text-gray-500 dark:text-gray-400">
+            Desculpe, não conseguimos encontrar essa página. Você encontrará
+            muito para explorar na página inicial.{' '}
+          </p>
+          <Button onClick={() => navigate('/')}>Volte para o Dashboard</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
