d037c7f07a050e9069869e4a9e921e558766b13b
paulo <paulo_vicali@icloud.com>
2024-06-21 15:04:34 -0300

    perf(main): adição de instâncias do axios
    Configuramos uma instância do axios para definir interceptors para permitir o uso de lógica
    personalizada antes de uma resposta ser processada ou antes de uma solicitação ser enviada.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,43 @@
-import axios from 'axios'
+import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios'
 
 import { env } from '@/env'
 
-export const api = axios.create({
+export const api: AxiosInstance = axios.create({
   baseURL: env.VITE_BASE_URL,
+  timeout: 1000,
 })
+
+api.interceptors.response.use(
+  (response: AxiosResponse): AxiosResponse => {
+    console.log('Resposta de sucesso:', response.status, response.statusText)
+    return response
+  },
+  (error) => {
+    if (axios.isAxiosError(error)) {
+      const axiosError = error as AxiosError
+
+      if (axiosError.response) {
+        // Erro na resposta da API
+        console.error(
+          'Erro na resposta da API:',
+          axiosError.response.status,
+          axiosError.response.statusText,
+        )
+      } else if (axiosError.request) {
+        // Erro na solicitação da API
+        console.error('Erro na solicitação da API:', axiosError.request)
+      } else {
+        // Erro genérico ou erro ao configurar solicitação da API
+        console.error(
+          'Erro na configuração da solicitação:',
+          axiosError.message,
+          error,
+        )
+      }
+    } else {
+      // Erro que não é do Axios
+      console.error('Erro desconhecido:', error)
+    }
+    return Promise.reject(error)
+  },
+)
