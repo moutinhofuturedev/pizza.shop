09c50ed7bb2c646fe58a3470df3de0e01533dd8d
paulo <paulo_vicali@icloud.com>
2024-06-21 14:51:35 -0300

    refactor(main): sincroniza apontamento da url de back-end com o front-end

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 8000,
-  },
 
   resolve: {
     alias: {
