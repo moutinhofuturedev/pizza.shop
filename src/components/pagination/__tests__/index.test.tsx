fe529830b86ce50774f3c2f06955a0f34187bf2c
paulo <paulo_vicali@icloud.com>
2024-07-29 13:11:08 -0300

    test(test/vitest): teste unitário com snapshot pagination

---
+++ src/components/pagination/__tests__/index.test.tsx
@@ -0,0 +1,68 @@
+import { render, screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
+
+import { Pagination } from '../index'
+
+const onPageChangeCallback = vi.fn()
+
+describe('<Pagination />', () => {
+  beforeEach(() => {
+    onPageChangeCallback.mockClear()
+  })
+
+  it('should display the right amount of pages and results', () => {
+    const { container } = render(
+      <Pagination
+        totalCount={200}
+        perPage={10}
+        pageIndex={0}
+        onPageChange={onPageChangeCallback}
+      />,
+    )
+
+    const totalResults = screen.getByText('Total de 200 item(s)')
+    const pageCount = screen.getByText('Página 1 de 20')
+
+    expect(pageCount).toBeInTheDocument()
+    expect(totalResults).toBeInTheDocument()
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should be able to navigate to the next page', async () => {
+    const wrapper = render(
+      <Pagination
+        pageIndex={0}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallback}
+      />,
+    )
+
+    const nextPageButton = wrapper.getByRole('button', {
+      name: 'Próxima página',
+    })
+
+    await userEvent.click(nextPageButton)
+
+    expect(onPageChangeCallback).toHaveBeenCalledWith(1)
+  })
+
+  it('should be able to navigate to the previous page', async () => {
+    const wrapper = render(
+      <Pagination
+        pageIndex={1}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallback}
+      />,
+    )
+
+    const previousPageButton = wrapper.getByRole('button', {
+      name: 'Página anterior',
+    })
+
+    await userEvent.click(previousPageButton)
+
+    expect(onPageChangeCallback).toBeCalledWith(0)
+  })
+})
