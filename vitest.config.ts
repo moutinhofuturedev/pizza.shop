538b3edacdee359c605a3ffc8794b6f5790d5b47
paulo <paulo_vicali@icloud.com>
2024-07-29 13:09:56 -0300

    build(test/vitest): pacotes e configurações

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,21 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    setupFiles: ['/vitest/setup.ts'],
+    setupFiles: ['./vitest/setup.ts'],
     environment: 'happy-dom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**'],
+    passWithNoTests: true,
+    coverage: {
+      provider: 'istanbul',
+      exclude: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/cypress/**',
+        '**/coverage/**',
+        '**/public/**',
+        '**/pages/_layout/**',
+      ],
+    },
   },
 })
