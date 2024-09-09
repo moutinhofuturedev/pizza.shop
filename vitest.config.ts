ccbb8ef632b05e82fe8d6dad125fc4024be69313
paulo <paulo_vicali@icloud.com>
2024-09-09 00:19:37 -0300

    feat(main): novo relatório two

--- vitest.config.ts
+++ vitest.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
       all: true,
       enabled: true,
       reporter: ['text', 'lcov'], // Gera o relatório LCOV necessário para o SonarCloud
-      reportsDirectory: './coverage', // Diretorio para salvar os arquivos de cobertura
+      reportsDirectory: 'coverage', // Diretorio para salvar os arquivos de cobertura
       include: [
         'src/**/*.{ts,tsx}',
         '__mocks__/**',
