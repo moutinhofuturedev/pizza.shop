bd74fbf91fbda8f6ebab94c7f7e4e45011da9c7d
paulo <paulo_vicali@icloud.com>
2024-09-21 19:19:39 -0300

    test(main): corrige teste

--- src/components/date-range-picker/__tests__/index.test.tsx
+++ src/components/date-range-picker/__tests__/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { DatePickerWithRange } from '../index'
 
 describe('DatePickerWithRange', () => {
@@ -13,18 +13,21 @@ describe('DatePickerWithRange', () => {
     expect(calendar).toBeInTheDocument()
   })
 
-  // it('renders with a date range selected', () => {
-  //   const onDateChange = vi.fn()
-  //   const date = { from: new Date('2022-01-01'), to: new Date('2022-01-31') }
+  it('renders with a date range selected', () => {
+    const onDateChange = vi.fn()
+    const date = {
+      from: new Date('2024-09-20T00:00:00.000Z'),
+      to: new Date('2024-09-27T00:00:00.000Z'),
+    }
 
-  //   const { container } = render(
-  //     <DatePickerWithRange date={date} onDateChange={onDateChange} />
-  //   )
+    const { container } = render(
+      <DatePickerWithRange date={date} onDateChange={onDateChange} />
+    )
 
-  //   const calendar = screen.getByText('Dec 31, 2021 - Jan 30, 2022')
-  //   fireEvent.click(calendar)
+    const calendar = screen.getByText('Sep 19, 2024 - Sep 26, 2024')
+    fireEvent.click(calendar)
 
-  //   expect(calendar).toBeInTheDocument()
-  //   expect(container).toMatchSnapshot()
-  // })
+    expect(calendar).toBeInTheDocument()
+    expect(container).toMatchSnapshot()
+  })
 })
