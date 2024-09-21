75f0f4ab1dafa0ac497b89281603b033a797f3ec
paulo <paulo_vicali@icloud.com>
2024-09-21 18:44:56 -0300

    test(main): adiciona teste unitário para date picker

---
+++ src/components/date-range-picker/__tests__/index.test.tsx
@@ -0,0 +1,34 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import { DatePickerWithRange } from '../index'
+
+describe('DatePickerWithRange', () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
+
+  it('renders with no date', () => {
+    const onDateChange = vi.fn()
+    const date = { from: undefined, to: undefined }
+
+    render(<DatePickerWithRange date={date} onDateChange={onDateChange} />)
+
+    const calendar = screen.getByText('Selecione uma data')
+
+    expect(calendar).toBeInTheDocument()
+  })
+
+  it('renders with a date range selected', () => {
+    const onDateChange = vi.fn()
+    const date = { from: new Date('2022-01-01'), to: new Date('2022-01-31') }
+
+    const { container } = render(
+      <DatePickerWithRange date={date} onDateChange={onDateChange} />
+    )
+
+    const calendar = screen.getByText('Dec 31, 2021 - Jan 30, 2022')
+    fireEvent.click(calendar)
+
+    expect(calendar).toBeInTheDocument()
+    expect(container).toMatchSnapshot()
+  })
+})
