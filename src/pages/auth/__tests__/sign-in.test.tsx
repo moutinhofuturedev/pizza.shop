1df64d9491d5bb9e94d8283f432db2dbb051c3bb
paulo <paulo_vicali@icloud.com>
2024-09-26 12:27:29 -0300

    test(main): testes unitários para sign in

--- src/pages/auth/__tests__/sign-in.test.tsx
+++ src/pages/auth/__tests__/sign-in.test.tsx
@@ -14,13 +14,16 @@ import { MemoryRouter } from 'react-router-dom'
 
 import { queryClient } from '@/lib/react-query'
 
+import { toast } from 'sonner'
 import { SignIn } from '../sign-in'
 
 const handleSubmitForm = vi.fn()
+const authenticateMock = vi.fn()
 
 describe('<SignIn />', () => {
 	beforeEach(() => {
 		handleSubmitForm.mockClear()
+		authenticateMock.mockClear()
 	})
 
 	it('should set default email input value if email is present on search params', () => {
@@ -72,6 +75,71 @@ describe('<SignIn />', () => {
 		const errorMessage = await screen.findByText(/e-mail inválido/i)
 
 		expect(errorMessage).toBeInTheDocument()
+	})
+
+	it('deve enviar o formulário e mostrar um toast de sucesso', async () => {
+		handleSubmitForm.mockImplementation(() => Promise.resolve())
+
+		render(<SignIn />, {
+			wrapper: ({ children }) => {
+				return (
+					<HelmetProvider>
+						<QueryClientProvider client={queryClient}>
+							<MemoryRouter initialEntries={['/user/sign-in']}>
+								{children}
+							</MemoryRouter>
+						</QueryClientProvider>
+					</HelmetProvider>
+				)
+			},
+		})
+
+		const emailInput = screen.getByLabelText('Seu e-mail')
+		const submitButton = screen.getByRole('button', {
+			name: /acessar painel/i,
+		})
+
+		await userEvent.type(emailInput, 'Jr5wA@example.com')
+		await userEvent.click(submitButton)
+
+		waitFor(() => {
+			expect(handleSubmitForm).toHaveBeenCalled()
+		})
+	})
+
+	it('deve mostrar um toast de erro se a autenticação falhar', async () => {
+		authenticateMock.mockRejectedValueOnce(
+			new Error('Erro ao enviar o link de autenticação'),
+		)
+
+		render(<SignIn />, {
+			wrapper: ({ children }) => (
+				<HelmetProvider>
+					<MemoryRouter
+						initialEntries={['/auth/sign-in?email=test@example.com']}
+					>
+						<QueryClientProvider client={queryClient}>
+							{children}
+						</QueryClientProvider>
+					</MemoryRouter>
+				</HelmetProvider>
+			),
+		})
+
+		const emailInput = screen.getByLabelText('Seu e-mail')
+		const submitButton = screen.getByRole('button', { name: /acessar painel/i })
+
+		await userEvent.type(emailInput, 'test@example.com')
+		await userEvent.click(submitButton)
+
+		waitFor(() => {
+			expect(authenticateMock).toHaveBeenCalledWith({
+				email: 'test@example.com',
+			})
+			expect(toast.error).toHaveBeenCalledWith(
+				'Erro ao enviar o link de autenticação',
+			)
+		})
 	})
 
 	it('should render the "Novo estabelecimento" link', () => {
