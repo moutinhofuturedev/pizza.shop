a29702f4080a788d35e6225ef1e495374c351bdc
paulo <paulo_vicali@icloud.com>
2024-10-02 23:33:20 -0300

    refactor(main): refatora teste

--- src/pages/app/orders/modules/__tests__/order-status.test.tsx
+++ src/pages/app/orders/modules/__tests__/order-status.test.tsx
@@ -1,21 +1,21 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import { OrderStatus, OrderStatusType } from '../order-status'
 
 describe('<OrderStatus />', () => {
 	it('should render display order status correctly with props', () => {
-		const wrapper = render(<OrderStatus status={OrderStatusType.Delivered} />)
+		render(<OrderStatus status={OrderStatusType.Delivered} />)
 
-		const statusText = wrapper.getByText('Entregue')
+		const statusText = screen.getByText('Entregue')
 
 		expect(statusText).toBeInTheDocument()
 	})
 
 	it('should render display order status correctly with class badge color', () => {
-		const wrapper = render(<OrderStatus status={OrderStatusType.Canceled} />)
+		render(<OrderStatus status={OrderStatusType.Canceled} />)
 
-		const statusText = wrapper.getByText('Cancelado')
-		const orderStatus = wrapper.getByTestId('order-status')
+		const statusText = screen.getByText('Cancelado')
+		const orderStatus = screen.getByTestId('order-status')
 
 		expect(statusText).toBeInTheDocument()
 		expect(orderStatus).toHaveClass('bg-red-500')
