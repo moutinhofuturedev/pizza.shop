1f3d896a1e572a85ff0c9c45d42927c8b7ec5b8c
paulo <paulo_vicali@icloud.com>
2024-10-03 00:32:35 -0300

    test(main): more tests

--- src/pages/app/orders/modules/__tests__/order-details.test.tsx
+++ src/pages/app/orders/modules/__tests__/order-details.test.tsx
@@ -1,11 +1,36 @@
 import { Dialog } from '@/components/ui/dialog'
 import { queryClient } from '@/lib/react-query'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { render, waitFor } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { HelmetProvider } from 'react-helmet-async'
 import { MemoryRouter } from 'react-router-dom'
 import { OrderDetails } from '../order-details'
 import { OrderStatusType } from '../order-status'
+
+const mockOrder = {
+	customer: {
+		name: 'John Doe',
+		phone: '123456789',
+		email: 'john@example.com',
+	},
+	createdAt: new Date(),
+	orderItems: [
+		{
+			id: 1,
+			product: { name: 'Product 1' },
+			quantity: 2,
+			priceInCents: 1000,
+		},
+	],
+	totalInCents: 2000,
+}
+
+const orderDetailsMock = {
+	...mockOrder,
+	status: OrderStatusType.Delivered,
+	orderId: '123',
+	open: true,
+}
 
 describe('<OrderDetails />', () => {
 	it('should render order details correctly', async () => {
@@ -14,19 +39,13 @@ describe('<OrderDetails />', () => {
 				<HelmetProvider>
 					<MemoryRouter>
 						<Dialog open={true}>
-							<OrderDetails
-								orderId='123'
-								status={OrderStatusType.Delivered}
-								open={true}
-							/>
+							<OrderDetails {...orderDetailsMock} />
 						</Dialog>
 					</MemoryRouter>
 				</HelmetProvider>
 			</QueryClientProvider>,
 		)
 
-		await waitFor(() => {
-			expect(container).toBeInTheDocument()
-		})
+		expect(container).toBeInTheDocument()
 	})
 })
